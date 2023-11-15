406b20753dbdf3ebcb406e920508781dd5d742fc
Liam Green-Hughes <601743+liamgh@users.noreply.github.com>
2023-11-15 22:03:06 +0000

    Refactor code

--- genquiz.py
+++ genquiz.py
@@ -1,20 +1,22 @@
 from openai import OpenAI
 import io
 
+# Change these settings to your preferences
+model_name="gpt-4-1106-preview"
+topic="1980s British computer products and companies"
+language="British English"
+# End
+
 client = OpenAI()
 message_stack =  [{"role": "system", "content": "You are a helpful assistant."}]
 
-model_name="gpt-4-1106-preview"
-topic="Gravesend, UK"
-language="English (UK)"
-
 initial_prompt = """
-Create a %s-themed radio quiz show in %s script with a host and three contestants. 
-Begin with introductions. Use the structure below, inserting sound effect placeholders and ensuring contestants occasionally answer incorrectly, allowing others to earn bonus points. Use real answers for questions. Only use specified options for SFX. Do not add descriptions of what is happening.
-There are no commercial breaks. Write out numbers or years in dialogue as text.
-Structure:
+Create a %s themed radio quiz show in %s script with a host and three contestants. 
+Begin with introductions. Use the structure below, inserting sound effect placeholders and ensuring contestants occasionally answer incorrectly, allowing others to earn bonus points. Only use real questions and answers. Only use specified options for SFX. Do not add descriptions of what is happening.
+There are no commercial breaks. Write out numbers or years in dialogue as text. Do not end the quiz or say goodbye until told to.
+Use this structure and respect newlines:
 [SFX:music|buzzer|correct|wrong]
-[HOST (male_female)|CONTESTANT (male|female) number]
+[HOST (male|female)|CONTESTANT (male|female) 1|2|3]
 text
 """
 
@@ -25,6 +27,7 @@ def call_completions(message_content):
         messages=message_stack
     )
     gentext = response.choices[0].message.content
+    print(gentext)
     message_stack.append({"role": "assistant", "content": gentext})
     return gentext
 
@@ -32,4 +35,11 @@ with io.open("output/script.txt", "w", encoding='utf8') as f:
     f.write(call_completions(initial_prompt % (topic, language)))
     f.write(call_completions("Generate the next round"))
     f.write(call_completions("Generate the last round"))
+    f.write(call_completions("End quiz with score, the winner and thank yous. Say goodbye until next time."))
     f.close()
+
+# Copyright Liam Green-Hughes 2023
+#
+#This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
--- performquiz.py
+++ performquiz.py
@@ -4,100 +4,128 @@ import random
 import shutil 
 from pathlib import Path
 from openai import OpenAI
+from pydub import AudioSegment
+import time
 
 client = OpenAI()
 
-# TODO Change to a class
+class PerformQuiz:
+    voices = {'male': ("alloy", "echo", "fable", "onyx"), 'female': ("alloy", "fable", "nova", "shimmer")}
+    allocated_voices = {}
+    current_voice = ""
+    used_voices = ["none"]
+    file_id = 0
+    scriptfile = ""
+    line = ""
+    files = []
 
-male_voices = ("alloy", "echo", "fable", "onyx")
-female_voices = ("alloy", "fable", "nova", "shimmer")
-allocated_voices = {}
-current_voice = ""
-used_voices = ["none"]
-file_id = 0
+    def __init__(self, scriptfile) -> None:
+        self.scriptfile = scriptfile
 
-def change_voice(line):
-    global current_voice
-    current_voice = line
-    if current_voice not in allocated_voices:
-        # allocate voice
-        print("Need to allocate voice for ", line)
-        allocated_voices[current_voice] = allocate_voice(line)
-        print(allocated_voices)
+    def change_voice(self):
+        person = self.parse_speaker_id()
+        if person["hash"] not in self.allocated_voices:
+            # allocate voice
+            self.allocated_voices[person["hash"]] = self.allocate_voice()
+        self.current_voice = person["hash"]
 
-def speak(line):
-    global current_voice
-    global file_id
-    print(current_voice, "/", allocated_voices[current_voice], ": ", line)
-    speech_file_path = "output/part%d.mp3" % (file_id)
-    response = client.audio.speech.create(
-        model="tts-1",
-        voice=allocated_voices[current_voice],
-        input=line
-    )
-    response.stream_to_file(speech_file_path)
-    file_id= file_id +1
+    def get_output_part_path(self, ext):
+        self.file_id = self.file_id +1
+        filename =  "output/part%02d.%s" % (self.file_id, ext)
+        self.files.append(filename)
+        return filename
 
-def parse_speaker_id(line):
-    person = {"id": "", "gender": "", "number": 0}
-    parts = re.search(r"\[(CONTESTANT|HOST) \((male|female)\)\s?(\d)?\]", line)
-    if parts != None:
-        person["id"] = parts.group(1)
-        person["gender"] = parts.group(2)
-        if len(parts.groups()) == 3:
-            person["number"] = parts.group(3)
-    return person
+    def speak(self):
+        print(self.current_voice, "/", self.allocated_voices[self.current_voice], ": ", self.line)
+        response = client.audio.speech.create(
+            model="tts-1",
+            voice= self.allocated_voices[self.current_voice],
+            input=self.line
+        )
+        response.stream_to_file(self.get_output_part_path("mp3"))       
+        # wait a little whicle to space out requests to the API
+        print("API rest")
+        time.sleep(2)
 
-def allocate_voice(line):
-    voice = "none"
-    person = parse_speaker_id(line)
-    while voice in used_voices:
-        if person["gender"] == "female":
-            voice = random.choice(female_voices)
-        else:
-            voice = random.choice(male_voices)
-    used_voices.append(voice)
-    return voice   
-
-def process_line(line):
-    line = line.strip()
-    if line == "":
-        return 
+    def parse_speaker_id(self):
+        person = {"id": "", "gender": "", "number": 0}
+        parts = re.search(r"\[(CONTESTANT|HOST) \((male|female)\)\s?(\d)?", self.line)
+        if parts != None:
+            person["id"] = parts.group(1)
+            person["gender"] = parts.group(2)
+            if len(parts.groups()) == 3 and parts.group(3) is not None and parts.group(3).isnumeric():
+                person["number"] = parts.group(3)
+        person["hash"] = "%s-%s-%s" % (person["id"], person["gender"], person["number"])
+        return person
     
-    if line.startswith("[SFX:music"):
-        play_music()
-    elif line.startswith("[SFX:buzzer]"):
-        add_buzzer()
-    elif line.startswith("[SFX:correct]"):
-        add_correct_sound()
-    elif line.startswith("[SFX:wrong]"):
-        add_wrong_sound()
-    elif line.startswith("[HOST "):
-        change_voice(line)
-    elif line.startswith("[CONTESTANT "):
-        change_voice(line)
-    else:
-        speak(line)
+    def allocate_voice(self):
+        voice = "none"
+        person = self.parse_speaker_id()
+        for cvoice in self.voices[person["gender"]]:
+            if cvoice not in self.used_voices and voice == "none":
+                voice = cvoice
+        self.used_voices.append(voice)
+        return voice 
+    
+    def play_music(self):
+        self.add_file("sfx-music-betweenrounds.wav")
 
-def play_music():
-    add_file("sfx-music-betweenrounds.mp3","mp3")
+    def add_buzzer(self):
+        self.add_file("chime.wav")
 
-def add_buzzer():
-    add_file("chime.wav","wav")
+    def add_correct_sound(self):
+        self.add_file("correct.wav")
 
-def add_correct_sound():
-    add_file("correct.wav","wav")
+    def add_wrong_sound(self):
+        self.add_file("wrong.wav")
 
-def add_wrong_sound():
-    add_file("wrong.wav","wav")
+    def add_file(self, source):
+        ext = source[source.rfind(".")+1:]
+        shutil.copyfile(source, self.get_output_part_path(ext))
+    
+    def process_line(self):
+        line = self.line.strip().replace(" ", "")
+        if line == "":
+            return 
+        
+        if line.startswith("[SFX:music"):
+            self.play_music()
+        elif line.startswith("[SFX:buzzer]"):
+            self.add_buzzer()
+        elif line.startswith("[SFX:correct]"):
+            self.add_correct_sound()
+        elif line.startswith("[SFX:wrong]"):
+            self.add_wrong_sound()
+        elif line.startswith("[HOST"):
+            self.change_voice()
+        elif line.startswith("[CONTESTANT"):
+            self.change_voice()
+        elif not line.startswith("["):
+            self.speak()
+        else:
+            print("Unrecognised command: ", line)
 
-def add_file(source, ext):
-    global file_id
-    shutil.copyfile(source, "output/part%d.%s" % (file_id, ext))
-    file_id = file_id + 1
+    def combine_files(self):
+        combined_sound = AudioSegment.silent(duration=1)
+        for partfile in self.files:
+            print("Combining:", partfile)
+            combined_sound = combined_sound + AudioSegment.from_file(partfile)
+        combined_sound.export("output/00aiquiz.mp3", format="mp3")
 
-with io.open("output/script.txt", "r", encoding='utf8') as f:
-    for line in f.readlines():
-        print(line)
-        process_line(line)
-    f.close()
+    def start(self):
+        with io.open(self.scriptfile, "r", encoding='utf8') as f:
+            for line in f.readlines():
+                self.line = line.strip()
+                self.process_line()
+            f.close()
+        self.combine_files()
+
+perform = PerformQuiz("output/script.txt")
+perform.start()
+
+
+# Copyright Liam Green-Hughes 2023
+#
+#This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.                 
